922084cf9caf58389e51c4b7f8106a0f2a360a17
cjadsonsantos <cjadsonsantos@hotmail.com>
2023-06-08 18:48:46 -0300

    'changes'

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="bg-gray-100">
-
+    <div class="container mx-auto bg-gray-300 p-8">
+        <h1 class="font-bold text-gray-600 text-lg">Hello, Tailwind 3!</h1>
     </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,14 @@
 module.exports = {
+    content: ["./components/**/*.{js,vue,ts}", "./layouts/**/*.vue", "./pages/**/*.vue", "./plugins/**/*.{js,ts}", "./nuxt.config.{js,ts}",],
     theme: {
-        screens: {
-            sm: '640px',
-            md: '768px',
-            lg: '1024px',
-            xl: '1280px',
-        },
-        extend: {
+        fontFamily: {
+            sans: ['Poppins', 'sans-serif'],
+        }, screens: {
+            sm: '640px', md: '768px', lg: '1024px', xl: '1280px',
+        }, extend: {
             colors: {
                 linkedin: {
-                    primary: '#0A66C2',
-                    lighter: '#378fe9',
-                    darker: '#004182',
+                    primary: '#0A66C2', lighter: '#378fe9', darker: '#004182',
                 },
             },
         },
