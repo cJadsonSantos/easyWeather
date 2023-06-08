fa3c0fa191e19f3a312f79262982b4a9aefc54d9
cjadsonsantos <cjadsonsantos@hotmail.com>
2023-06-08 20:21:27 -0300

    'changes'

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,46 +1,59 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { resolve } from "path";
+import {resolve} from "path";
 
 export default defineNuxtConfig({
-  head: {
-    title: "Easy Weather",
-    htmlAttrs: {
-      lang: "pt-br"
+    head: {
+        title: "Easy Weather",
+        htmlAttrs: {
+            lang: "pt-br"
+        },
+        meta: [
+            {charset: "utf-8"},
+            {name: "viewport", content: "width=device-width, initial-scale=1"},
+            {hid: "description", name: "description", content: ""},
+            {name: "format-detection", content: "telephone=no"}
+        ],
+        link: [
+            {rel: "icon", type: "image/x-icon", href: "/favicon.ico"},
+            {
+                rel: "stylesheet",
+                href: "https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap",
+            }
+        ]
     },
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
-      { name: "format-detection", content: "telephone=no" }
-    ],
-    link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-      {
-        rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap",
-      },
-    ],
-  },
 
-  buildModules: ["@nuxtjs/eslint-module"],
-  build: {
-    postcss: {
-      postcssOptions: {
-        plugins: {
-          tailwindcss: {},
-          autoprefixer: {}
+    buildModules: [
+        "@nuxtjs/eslint-module",
+        '@nuxtjs/fontawesome'
+    ],
+    fontawesome: {
+        component: 'fa',
+        icons: {
+            solid: true,
+            brands: true
         }
-      }
-    }
-  },
-  css: ["~/assets/css/tailwind.css"],
-  alias: {
-    "@": resolve(__dirname, "/")
-  },
-  devtools: { enabled: true },
-  modules: [
-    // ...
-    "@nuxtjs/tailwindcss",
-    "@pinia/nuxt"
-  ],
+    },
+    build: {
+        postcss: {
+            postcssOptions: {
+                plugins: {
+                    tailwindcss: {},
+                    autoprefixer: {}
+                }
+            }
+        }
+    },
+    css: [
+        "~/assets/css/tailwind.css",
+        '@fortawesome/fontawesome-svg-core/styles.css'
+    ],
+    alias: {
+        "@": resolve(__dirname, "/")
+    },
+    devtools: {enabled: true},
+    modules: [
+        // ...
+        "@nuxtjs/tailwindcss",
+        "@pinia/nuxt"
+    ],
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,22 @@
 <template>
     <div class="flex flex-col min-h-screen bg-[#0E1A2A]">
+        <header class="sticky top-0 bg-21394F"></header>
+        <div class="text-white">
+            <div class="flex items-center gap-3 flex-1">
+                <font-awesome-icon :icon="['fas','sun']"/>
+                <p class="text-2xl">The Local Weather</p>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+
 export default {
     name: "index",
+    components: {FontAwesomeIcon},
 };
 </script>
 
