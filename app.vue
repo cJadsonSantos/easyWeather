41bc2005ad161daacaa330fbeeb3d983b05dd9aa
cjadsonsantos <cjadsonsantos@hotmail.com>
2023-06-08 18:07:10 -0300

    'changes'

--- app.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <NuxtLink
-            to="/"
-            class="font-light text-gray-500 hover:text-gray-400 duration-100"
-    >
-        Home
-    </NuxtLink>
-</template>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="bg-gray-100">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
