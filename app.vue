914b8ed6379bb6c9ceb40588265328cc3bad70ed
cjadsonsantos <cjadsonsantos@hotmail.com>
2023-06-08 16:51:27 -0300

    'changes'

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+    <NuxtLink
+            to="/"
+            class="font-light text-gray-500 hover:text-gray-400 duration-100"
+    >
+        Home
+    </NuxtLink>
 </template>
