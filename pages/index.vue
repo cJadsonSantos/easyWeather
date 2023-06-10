c07ecb7fed23bb68e3eb3cef37333b10a8d65ab9
cjadsonsantos <cjadsonsantos@hotmail.com>
2023-06-09 23:10:30 -0300

    'changes'

--- pages/index.vue
+++ pages/index.vue
@@ -2,23 +2,34 @@
     <div v-if="show"
          class="flex flex-col h-screen min-h-screen bg-gradient-to-r from-[#0E1A2A] to-[#21394F] text-[#F3F3F3]">
         <div class="h-1/2 flex flex-col items-center justify-center">
+
             <div class="text-center mt-12 items-center space-y-12">
                 <font-awesome-icon :icon="['fas','cloud-sun']" class="text-7xl" bounce/>
                 <p class="text-4xl font-bold">{{ weather.city }}</p>
             </div>
+
             <div class="text-center space-y-2 mt-4">
                 <p class="text-5xl font-bold">{{ `${weather.temp_c}º` }}</p>
                 <p class="text-2xl text-gray-400">{{ weather.condition }}</p>
             </div>
-        </div>
 
+            <input
+                    type="text"
+                    v-model="city"
+                    placeholder="Search City.."
+                    class="mt-4 px-4 py-2 border border-none rounded-full bg-[#0E1A2A] text-[#F3F3F3]"
+                    @input="searchWeather"
+            />
 
-        <div>
-            <ul class="divide-y divide-[#F3F3F3] p-2">
-                <li class="py-2">Dia 1</li>
-                <li class="py-2">Dia 2</li>
-                <li class="py-2">Dia 3</li>
-            </ul>
+            <div class="flex flex-wrap w-80 space-x-2 space-y-2 mt-4">
+                <template v-for="day in currencyDayForecastHour" :key="day.id">
+                    <font-awesome-icon :icon="['fas','cloud']" class="text-1xl p-2" fade/>
+                    <div class="ml-3 text-left text-xs">
+                        <p>{{ day.time.slice(11, 16) }}</p>
+                        <p>{{ `${day.temp_c}º` }}</p>
+                    </div>
+                </template>
+            </div>
         </div>
     </div>
 </template>
@@ -35,12 +46,18 @@ export default {
         return {
             show: false,
             weather: {},
+            currencyDayForecastHour: {},
+
+            city: null,
+            currentDate: null,
+
             weatherBaseRoute: 'https://api.weatherapi.com/v1',
             weatherKey: '953c9a5eae1041bca6b195920230806'
         }
     },
     created() {
         this.getCurrentLocation();
+        this.getCurrentDate();
     },
     methods: {
         async getCurrentLocation() {
@@ -58,7 +75,7 @@ export default {
 
         async getWeather(latitude, longitude) {
             try {
-                await axios.get(`${this.weatherBaseRoute}/current.json`, {
+                await axios.get(`${this.weatherBaseRoute}/forecast.json`, {
                     params: {
                         key: this.weatherKey,
                         q: `${latitude},${longitude}`,
@@ -69,16 +86,40 @@ export default {
                     }
                 }).then((response) => {
                     this.weather.city = response.data.location.name
-                    this.weather.country = response.data.location.country
                     this.weather.condition = response.data.current.condition.text
                     this.weather.temp_c = response.data.current.temp_c
                     this.show = true
-                    console.log(response.data);
+
+                    this.currencyDayForecastHour = response.data.forecast.forecastday[0].hour
+                    console.log(response.data.forecast.forecastday[0].hour);
                 })
             } catch (error) {
                 console.error(error);
             }
+        },
+
+        async searchWeather() {
+            await axios.get(`${this.weatherBaseRoute}/current.json`, {
+                params: {
+                    key: this.weatherKey,
+                    q: `${this.city}`,
+                }
+            }).then(response => {
+                this.weather.city = response.data.location.name;
+                this.weather.condition = response.data.current.condition.text
+                this.weather.temp_c = response.data.current.temp_c
+                console.log(response.data)
+            })
+        },
+
+        getCurrentDate() {
+            const today = new Date();
+            const year = today.getFullYear();
+            const month = String(today.getMonth() + 1).padStart(2, '0');
+            const day = String(today.getDate()).padStart(2, '0');
+            this.currentDate = `${day}/${month}/${year}`;
         }
+
     },
 };
 </script>
